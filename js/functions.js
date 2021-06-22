5d8f6b7db1aee561011d0a939ac40940c528d66c
Cleverson Lira <cleverson_lira@outlook.com>
2021-06-22 14:29:27 -0300

    rebase onto master

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,13 @@
-var myHeaders = new Headers();
-
-myHeaders.set('Content-Type', 'text/html');
-myHeaders.append('Content-Type', 'application/javascript');
-myHeaders.append('Content-Type', 'charset=utf-8');
-
-
 const buttonMenuMobile = document.querySelector('.button-menu-mobile');
 
 function toggleMenu() {
     const nav = document.querySelector('.header-nav');
     nav.classList.toggle('active');
     
-    if(!nav.classList.contains('active')) {
-        buttonMenuMobile.innerHTML='|||';
-        
+    if(nav.classList.contains('active')) {
+        buttonMenuMobile.textContent="X";
     } else {
-        buttonMenuMobile.innerHTML='&#10005';
+        buttonMenuMobile.textContent="|||";
     }
 }
 
